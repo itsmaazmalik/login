792e12ef5df51ec4ebf8f8ce691ffe8de832d1b2
Maaz Malick <itsmaazmalick@gmail.com>
2022-10-07 21:38:14 +0500

    Upload Initial Files in Login Form

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var innerCard = document.getElementById("inner-card");
+// ------------------- Function to Open Register Card ------------------
+function openRegister(){
+    innerCard.style.transform = "rotateY(-180deg)";
+}
+// ------------------- Function to Open Login Card ------------------
+function openLogin(){
+    innerCard.style.transform = "rotateY(0deg)";
+}
